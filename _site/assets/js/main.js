8649ad0187c07e3c6ed55f48ad0be6ef8710afdf
Ben Bernheim <ben@bernheim.net>
2019-08-09 20:23:53 +0300

    responsiveness improved

---
+++ _site/assets/js/main.js
@@ -0,0 +1,11 @@
+//Henri, if you're reading this, I'm sorry I'm such a mediocre disappointment
+
+var swiper = new Swiper('.swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+      });
